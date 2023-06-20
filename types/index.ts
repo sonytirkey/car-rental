2c11d8f1abd5d761a1c440e4c636b421f3a88ca8
sonytirkey <tirkey.sony.01@gmail.com>
2023-06-20 18:28:57 +0530

    initial commit

--- types/index.ts
+++ types/index.ts
@@ -4,7 +4,7 @@ export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: "button" | "submit";
+    btnType?: "button" | "submit";  
     textStyles?: string;
     rightIcon?: string;
     isDisabled?: boolean;
